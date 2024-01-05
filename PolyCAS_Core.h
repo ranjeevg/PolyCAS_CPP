d1b94f132f22c5f2d99a3b249c598b818c783e24
Ranjeev Grewal <ranjeev.grewal1349@gmail.com>
2024-01-05 13:00:05 -0800

    work on the Polynomial class (which is now a std::vector<Term>, where Term is just a placeholder class to represent the coeff. and power of a single algebraic term)

--- PolyCAS_Core.h
+++ PolyCAS_Core.h
@@ -5,6 +5,7 @@
 #include <stack>
 #include <string>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
@@ -12,5 +13,41 @@ class Polynomial
 {
 public:
 	// empty polynomial
-	Polynomial() {}
+	Polynomial() 
+	{
+		// doesn't do anything for now
+	}
+
+	// constructor overload for a complete expression
+	Polynomial(vector<Term> expr) 
+	{
+		Expression = expr;
+	}
+
+	// apparently C++ doesn't have extension functions, so declaring a function within the class itself to simplify like terms
+	Polynomial Simplify() 
+	{
+		Polynomial simplified = Polynomial();
+		return simplified;
+	}
+
+protected:
+	// a vector (AKA list, in C# terminology) of terms
+	vector<Term> Expression;
+
+// for operations and the like
+// TODO: calculus operators
+private: 
+	Polynomial Add(Polynomial p);
+	Polynomial Subtract(Polynomial p);
+	Polynomial Multiply(Polynomial p);
+	Polynomial Divide(Polynomial p);
+};
+
+class Term {
+public: 
+	// the coefficient of the term
+	double TermCoefficient;
+	// term degree
+	int Power;
 };
